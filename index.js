3e022b5ae4a748ff55ec1daf8ce2e694a22fca80
Simrat Grewal <simrats169169@gmail.com>
2024-02-10 17:30:01 -0800

    Initial chatgpt api implementation

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const api_route = require("./routes/api.js");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded());
+
 app.use("/api", api_route);
 
 app.listen(PORT, () => {
+  if (!process.env.OPENAI_API_KEY) {
+    console.error("No open ai api key found");
+  }
   console.log(`Started listening on port ${PORT}`);
 });
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const OpenAI = require("openai");
 
 const router = express.Router();
 
-router.get("/script", (req, res) => {
+router.get("/script", async (req, res) => {
+  const open = new OpenAI.OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const topic = req.query.topic;
+  if (!topic) {
+    return res.status(400).json({ error: "No topic provided" });
+  }
+
+  const completion = await open.chat.completions.create({
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {
+        role: "user",
+        content: `Give a 100 word youtube video script on the following topic: ${topic}.`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion);
+
   return res.json({
     scripts: ["lor"],
   });
